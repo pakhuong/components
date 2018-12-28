19dda68c8745c924b04658f8419dd6184ef5d761
pakhuong <anhkhuong1602@gmail.com>
2018-12-28 23:43:50 +0700

    Make component configurable with props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,19 @@
-import faker from 'faker';
 import React from 'react';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}></img>
+                <img alt="avatar" src={props.avatar}></img>
             </a>
             <div className="content">
                 <a href="/" className="author">
-                    Sam
-                    </a>
+                    {props.author}
+                </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{props.timeAgo}</span>
                 </div>
-                <div className="text">Nice blog post!</div>
+                <div className="text">{props.content}</div>
             </div>
         </div>
     );
